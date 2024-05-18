a27d36ac464640a871ef56195dc382cd88910a66
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-18 14:36:25 +0530

    search bar

--- src/Components/productlist.jsx
+++ src/Components/productlist.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import ProductCard from "./productcard";
 
 
 export default function Productlist({ catagory }) {
       const [products, setProducts] = useState([]);
+      const [searchText, updateSeacrchText] = useState('');
 
       useEffect(
             () => {
@@ -17,8 +18,20 @@ export default function Productlist({ catagory }) {
             let resource = await fetch(`https://fakestoreapi.in/api/products/category?type=${catagory}`);
             let productslist = await resource.json()
             setProducts(productslist.products)
-            // console.log(productslist.products)
       }
+
+      /*Search */
+      const filteredProducts = useMemo(
+            () => {
+
+                  return products.filter(
+                        (p) => {
+                              return p.title.toLowerCase().includes(searchText)
+                        }
+                  )
+            }, [products, searchText]
+      )
+      /*Search */
 
       if (products?.length == 0) {
             return (<h1>Fetching data....</h1>)
@@ -26,9 +39,10 @@ export default function Productlist({ catagory }) {
 
       return (
             <>
+                  <div className="searchbar"><input style={{ width: "250px", height: "30px", borderRadius: '15px', fontFamily: "cursive", fontSize: 16 }} type="text" value={searchText} onChange={e => updateSeacrchText(e.target.value)} /></div>
                   <h1>Products fetching through Api Using React </h1>
                   <div className="product-list">
-                        {products?.map((p) => <ProductCard {...p} key={p.id}></ProductCard>)}
+                        {filteredProducts?.map((p) => <ProductCard {...p} key={p.id}></ProductCard>)}
                   </div>
             </>
       )
